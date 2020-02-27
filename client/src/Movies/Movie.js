e617641951b3faca3bbf996e196cc1bc786b11ac
Marco Whyte <mwhyte@umass.edu>
2020-02-27 15:55:08 -0600

    mvp

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useRouteMatch } from 'react-router-dom';
-import MovieCard from './MovieCard';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useRouteMatch } from "react-router-dom";
+import MovieCard from "./MovieCard";
 
-function Movie({ addToSavedList }) {
+function Movie(props) {
   const [movie, setMovie] = useState(null);
   const match = useRouteMatch();
-
+  console.log("looky", props);
   const fetchMovie = id => {
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
@@ -15,9 +15,22 @@ function Movie({ addToSavedList }) {
   };
 
   const saveMovie = () => {
-    addToSavedList(movie);
+    props.addToSavedList(movie);
   };
-
+  const handleUpdate = e => {
+    e.preventDefault();
+    props.history.push(`/update-movie/${movie.id}`);
+  };
+  const handleDelete = e => {
+    e.preventDefault();
+    axios
+      .delete(`http://localhost:5000/api/movies/${movie.id}`)
+      .then(res => {
+        props.history.push("/");
+        window.location.reload(false);
+      })
+      .catch(err => console.log(err));
+  };
   useEffect(() => {
     fetchMovie(match.params.id);
   }, [match.params.id]);
@@ -27,12 +40,18 @@ function Movie({ addToSavedList }) {
   }
 
   return (
-    <div className='save-wrapper'>
+    <div className="save-wrapper">
       <MovieCard movie={movie} />
 
-      <div className='save-button' onClick={saveMovie}>
+      <div className="save-button" onClick={saveMovie}>
         Save
       </div>
+      <button onClick={handleUpdate} className="md-button">
+        Edit
+      </button>
+      <button onClick={handleDelete} className="md-button">
+        Delete
+      </button>
     </div>
   );
 }
